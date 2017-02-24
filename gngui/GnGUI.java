3ec71be20e3321da52bcb96ca023cd92de0aaadb
gandalf3 <zzyxpaw@gmail.com>
2017-02-24 00:31:01 -0800

    get buttons generated

--- gngui/GnGUI.java
+++ gngui/GnGUI.java
@@ -6,7 +6,11 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+
+import javafx.*;
+import javafx.geometry.*;
  
 public class GnGUI extends Application {
     
@@ -14,16 +18,21 @@ public class GnGUI extends Application {
     public void start(Stage primaryStage) {
         Button[] btns = new Button[10];
 	StackPane root = new StackPane();
-
+    HBox hb = new HBox();
+    hb.setPadding(new Insets(15, 12, 15, 12));
+    hb.setSpacing(10);
+    hb.setStyle("-fx-background-color: #395868;");
+        
 	for (int i=0; i<btns.length; i++) {
 		Button b = new Button();
-		b.setText(Integer.toString(i));
-
+		b.setText(Integer.toString(i+1));
+        b.setPrefSize(40, 20);
 		b.setTranslateX(i*10);
-		root.getChildren().add(b);
+		hb.getChildren().add(b);
 		btns[i] = b;
-
 	}
+    
+    root.getChildren().add(hb);
         /*
 	btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
@@ -34,9 +43,9 @@ public class GnGUI extends Application {
             }
         });
 	*/
-        
-       
- Scene scene = new Scene(root, 300, 250);
+    
+//TODO ew ew ew hardcoded ew.. 
+Scene scene = new Scene(root, (40*11)+(15*11), 20+12);
 
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
